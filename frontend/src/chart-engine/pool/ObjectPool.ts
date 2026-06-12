0d3f2e620043ababdc9968249280b7b72541b481
MIMO <lynat1k@list.ru>
2026-06-12 07:18:36 +0300

    feat: add generic object pool for chart engine
    - Create ObjectPool<T> class with acquire/release/releaseAll methods
    - Pre-allocate pool with configurable initial size
    - Zero allocations in render loop via object reuse
    - Part of Phase 5: Chart Engine Framework + Japanese Candles

---
+++ frontend/src/chart-engine/pool/ObjectPool.ts
@@ -0,0 +1,45 @@
+export class ObjectPool<T> {
+  private pool: T[] = [];
+  private active: T[] = [];
+  private factory: () => T;
+  private reset: (obj: T) => void;
+
+  constructor(factory: () => T, reset: (obj: T) => void, initialSize: number = 0) {
+    this.factory = factory;
+    this.reset = reset;
+    for (let i = 0; i < initialSize; i++) {
+      this.pool.push(this.factory());
+    }
+  }
+
+  acquire(): T {
+    const obj = this.pool.length > 0 ? this.pool.pop()! : this.factory();
+    this.active.push(obj);
+    return obj;
+  }
+
+  release(obj: T): void {
+    const idx = this.active.indexOf(obj);
+    if (idx !== -1) {
+      this.active.splice(idx, 1);
+      this.reset(obj);
+      this.pool.push(obj);
+    }
+  }
+
+  releaseAll(): void {
+    while (this.active.length > 0) {
+      const obj = this.active.pop()!;
+      this.reset(obj);
+      this.pool.push(obj);
+    }
+  }
+
+  get activeCount(): number {
+    return this.active.length;
+  }
+
+  get totalCount(): number {
+    return this.pool.length + this.active.length;
+  }
+}
