c158670ed59f5df76538ec4c3a59b89551d88125
MIMO <lynat1k@list.ru>
2026-06-29 15:13:20 +0300

    fix(clickhouse): TTL 100->10 YEAR (DateTime 2106 overflow wiped data)

--- backend/internal/repository/clickhouse/migrations/009_disable_data_ttl.sql
+++ backend/internal/repository/clickhouse/migrations/009_disable_data_ttl.sql
@@ -1,4 +1,4 @@
-ALTER TABLE clusters_futures MODIFY TTL toDateTime(candle_open) + INTERVAL 100 YEAR;
-ALTER TABLE clusters_spot MODIFY TTL toDateTime(candle_open) + INTERVAL 100 YEAR;
-ALTER TABLE bookdepth_ratio MODIFY TTL toDateTime(snapshot_ts) + INTERVAL 100 YEAR;
-ALTER TABLE long_short_ratio MODIFY TTL toDateTime(ts) + INTERVAL 100 YEAR;
+ALTER TABLE clusters_futures MODIFY TTL toDateTime(candle_open) + INTERVAL 10 YEAR SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE clusters_spot MODIFY TTL toDateTime(candle_open) + INTERVAL 10 YEAR SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE bookdepth_ratio MODIFY TTL toDateTime(snapshot_ts) + INTERVAL 10 YEAR SETTINGS materialize_ttl_after_modify = 0;
+ALTER TABLE long_short_ratio MODIFY TTL toDateTime(ts) + INTERVAL 10 YEAR SETTINGS materialize_ttl_after_modify = 0;
